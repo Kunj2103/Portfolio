2053d437db0fe787ad5dd896654d4e9bd593d44e
Kunj2103 <kunjjansari1@gmail.com>
2023-09-05 18:24:22 +0530

    Added browser language support

--- src/i18n.js
+++ src/i18n.js
@@ -2,16 +2,20 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import enTranslation from "./locales/en.json";
 import esTranslation from "./locales/es.json";
-i18n.use(initReactI18next).init({
-  resources: {
-    en: {
-      translation: enTranslation, // Loading en.json translation data
+import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
+i18n
+  .use(initReactI18next)
+  .use(I18nextBrowserLanguageDetector)
+  .init({
+    resources: {
+      en: {
+        translation: enTranslation, // Loading en.json translation data
+      },
+      es: {
+        translation: esTranslation, // Loading es.json translation data
+      },
     },
-    es: {
-      translation: esTranslation, // Loading es.json translation data
-    },
-  },
-  debug: true,
-  fallbackLng: "en",
-});
+    debug: true,
+    fallbackLng: "en",
+  });
 export default i18n;
