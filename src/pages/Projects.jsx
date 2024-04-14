74612fee79cbe1c912174bcdbad55faeca87f4cf
Kunj2103 <kunjjansari1@gmail.com>
2024-04-14 17:31:15 +0530

    added projects and tech stack

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -11,23 +11,22 @@ const Projects = () => {
         {t("projects.projectsInfo", {
           returnObjects: true,
         }).map((project, index) => (
-          <div
-            key={index}
-            className="card shadow mb-5 bg-white rounded"
-            style={{ width: "80%", textAlign: "center" }}
+          <a
+            href={project.siteUrl}
+            target="_blank"
+            className="text-decoration-none"
           >
-            <a href={project.siteUrl}>
-              <img
-                className="card-img-top"
-                src={project.image}
-                alt={project.name}
-              />
-            </a>
-            <div className="card-body">
-              <h5 className="card-title">{project.name}</h5>
-              <p className="card-text">{project.description}</p>
+            <div
+              key={index}
+              className="card shadow mb-5 bg-white rounded"
+              style={{ width: "80%", textAlign: "center" }}
+            >
+              <div className="card-body">
+                <h5 className="card-title">{project.name}</h5>
+                <p className="card-text">{project.description}</p>
+              </div>
             </div>
-          </div>
+          </a>
         ))}
       </div>
     </>
